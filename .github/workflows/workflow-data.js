7372aabc5f57d8a47782f4d5ee9ad05c007a47a2
KeshavamurthyGN <keshavamurthy.gn@sixt.com>
2023-09-11 18:28:46 +0530

    added new workflow

---
+++ .github/workflows/workflow-data.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const fs = require('fs');
+
+try {
+  const workflowStartDate = core.getInput('workflow-start-time');
+  const runnerName = core.getInput('runner-name');
+  var proj = {
+    startDate : workflowStartDate,
+    name: runnerName
+  };
+
+  const result = JSON.stringify(proj)
+  console.log(result);
+
+  try {
+     fs.writeFile('./workflow-report.json', result, (error) => {
+      if (error) {
+          throw error;
+      }
+      console.log("File has been created");
+    });
+  } catch {
+    core.info(`Could not read .xcode-version file. Using Xcode ${defaultVersion} as default.`);
+    core.setOutput('xcode_version', defaultVersion);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
